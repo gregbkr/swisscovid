55b0a44a845fdb66aa0c80eb583d0fee194a79c4
gregbkr <gregboccard@gmail.com>
2020-10-05 23:09:57 +0200

    Move DNS and use terraform.io workspace

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,59 +1,58 @@
 provider "aws" {
-  region     = "eu-west-3"
+  version = "~> 2.62"
+  region  = "eu-west-1"
+  profile = "gregbkr"
 }
 
-# Will we store our state in S3, and lock with dynamodb
 terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "terraform-up-and-running-state-gg"
-    key            = "covid/prod/terraform.tfstate"
-    region         = "eu-west-3"
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
+  required_version = "~> 0.12.0"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "gregbkr"
+    workspaces {
+      name = "swisscovid-prod"
+    }
   }
 }
 
 variable "dns_domain" {
-  default = "swisscovid.com"
+  default = "satoshi.tech"
 }
 variable "dns_record" {
-  default = ""
+  default = "swisscovid"
 }
 variable "cert_arn" {
-  default = "arn:aws:acm:us-east-1:282835178041:certificate/c6fef777-b34c-44a9-bb9d-b24b59fa8b0d"
+  default = "arn:aws:acm:us-east-1:282835178041:certificate/e34fadff-b0d8-47d4-978f-58bd41b6194a"
 }
 variable "iam_user" {
   default = "swisscovid.com-write-to-s3"
 }
 
-
 # CALL MODULES
 module "my_s3" {
-    source = "../modules/s3"
-    bucket = "${var.dns_domain}"
-		iam_user_arn = "${module.my_iam.iam_user_arn}"
-} 
+  source       = "../modules/s3"
+  bucket       = "${var.dns_record}.${var.dns_domain}"
+  iam_user_arn = "${module.my_iam.iam_user_arn}"
+}
 
 module my_iam {
-  source = "../modules/iam"
-  iam_user = "${var.dns_domain}-write-to-s3"
+  source     = "../modules/iam"
+  iam_user   = "${var.dns_record}.${var.dns_domain}-write-to-s3"
   bucket_arn = "${module.my_s3.bucket_arn}"
 }
 
 module my_cloudfront {
-	source = "../modules/cloudfront"
-	dns_domain = "${var.dns_domain}"
-	bucket_domain = "${module.my_s3.bucket_url}"
-	cert_arn = "${var.cert_arn}"
+  source        = "../modules/cloudfront"
+  dns_domain    = "${var.dns_record}.${var.dns_domain}"
+  bucket_domain = "${module.my_s3.bucket_url}"
+  cert_arn      = "${var.cert_arn}"
 }
 
 module my_route53 {
-  source = "../modules/route53"
-  dns_domain = "${var.dns_domain}"
-  dns_record = "${var.dns_record}"
-  bucket_url = "${module.my_cloudfront.domain_name}"
+  source         = "../modules/route53"
+  dns_domain     = "${var.dns_domain}"
+  dns_record     = "${var.dns_record}"
+  bucket_url     = "${module.my_cloudfront.domain_name}"
   bucket_zone_id = "${module.my_cloudfront.hosted_zone_id}"
 }
 
